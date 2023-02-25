4c45b972529ececa69cdf672bda51516a648d4b3
5r33n <5r33ns@gmail.com>
2023-02-25 19:54:41 +0000

    added addBeat function

--- contracts/Heart.sol
+++ contracts/Heart.sol
@@ -1,7 +1,24 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.17;
 
 contract Heart {
-    
+    event BeatAdded();
+
+    struct Beat {
+        address addr;
+        string data;
+        uint256 rhythm;
+    }
+
+    Beat[] public beats;
+
+    function addBeat(
+        address _addr,
+        string memory _data,
+        uint256 _rhythm
+    ) public {
+        beats.push(Beat({addr: _addr, data: _data, rhythm: _rhythm}));
+        emit BeatAdded();
+    }
 }
