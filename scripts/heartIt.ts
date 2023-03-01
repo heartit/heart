2092f340be15ca859645b5c80a0b30c9b889ed79
5r33n <5r33ns@gmail.com>
2023-03-01 16:15:06 +0000

    generalized functions

--- scripts/heartIt.ts
+++ scripts/heartIt.ts
@@ -5,7 +5,9 @@ let heart, signers, accounts
 async function main() {
   await initHeart("0x5FbDB2315678afecb367f032d93F642f64180aa3")
   //await addBeats(3, 1, "tree", 1000000)
-  await getBeats("tree")
+  //await getBeats("tree")
+  await addBeats(8, 8, "fossil", 1000000)
+  await getBeats("fossil")
 }
 main()
 
@@ -18,8 +20,8 @@ async function initHeart(_deployedAddr) {
   console.log("[OK] Initialized Heart")
 }
 
-async function addBeats(_n, _initSignerIndex, _data, _rhythm) {
-  for (let i = _initSignerIndex; i <= _n; i++) {
+async function addBeats(_fromSigner, _toSigner, _data, _rhythm) {
+  for (let i = _fromSigner; i <= _toSigner; i++) {
     const connectedSigner = heart.connect(signers[i])
     await connectedSigner.addBeat(_data, _rhythm)
   }
@@ -28,6 +30,6 @@ async function addBeats(_n, _initSignerIndex, _data, _rhythm) {
 }
 
 async function getBeats(_data) {
-  const beats = await heart.getBeats("tree")
+  const beats = await heart.getBeats(_data)
   console.log(beats)
 }
